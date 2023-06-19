91fef4fd7f7c9a9f1f556387ae7a0ad2f0832ae6
wwh <1041155121@qq.com>
2023-06-20 02:08:42 +0800

    5

--- manager/assets/header.js
+++
@@ -1,9 +0,0 @@
-const nodeInfoButton = document.getElementById('node-info');
-nodeInfoButton.addEventListener('click', () => {
-  location.assign('nodes.html');
-});
-
-const datasetInfoButton = document.getElementById('dataset-info');
-datasetInfoButton.addEventListener('click', () => {
-  location.assign('datasets.html');
-});
