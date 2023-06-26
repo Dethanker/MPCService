4d9eb7bf3f0b3b44122e2fc9e41c5106478b5dac
wwh <1041155121@qq.com>
2023-06-26 17:20:30 +0800

    13

--- key_management/keys_certificates/creatcerts.go
+++ key_management/keys_certificates/creatcerts.go
@@ -14,6 +14,8 @@ import (
 	"time"
 )
 
+var nextNodeNum = 1
+
 func main() {
 	// 读取根证书文件
 	rootCAFile := "RootCA.crt"
@@ -22,17 +24,20 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to read root CA file: %v", err)
 	}
+
 	rootCAKeyData, err := os.ReadFile(rootCAKeyFile)
 	if err != nil {
 		log.Fatalf("Failed to read root CA key file: %v", err)
 	}
 
+	block, _ := pem.Decode(rootCAData)
+	block2, _ := pem.Decode(rootCAKeyData)
 	// 解析根证书和私钥
-	rootCA, err := x509.ParseCertificate(rootCAData)
+	rootCA, err := x509.ParseCertificate(block.Bytes)
 	if err != nil {
 		log.Fatalf("Failed to parse root CA certificate: %v", err)
 	}
-	rootCAKey, err := x509.ParsePKCS1PrivateKey(rootCAKeyData)
+	rootCAKey, err := x509.ParsePKCS1PrivateKey(block2.Bytes)
 	if err != nil {
 		log.Fatalf("Failed to parse root CA private key: %v", err)
 	}
@@ -48,9 +53,9 @@ func main() {
 
 	// 定义证书模板
 	template := x509.Certificate{
-		SerialNumber: big.NewInt(0),
+		SerialNumber: big.NewInt(int64(nextNodeNum)),
 		Subject: pkix.Name{
-			CommonName: fmt.Sprintf("node_%d", time.Now().UnixNano()),
+			CommonName: fmt.Sprintf("node_%d", nextNodeNum),
 		},
 		NotBefore: time.Now(),
 		NotAfter:  time.Now().AddDate(1, 0, 0),
@@ -60,6 +65,8 @@ func main() {
 		IPAddresses: []net.IP{net.ParseIP("127.0.0.1")},
 	}
 
+	nextNodeNum++
+
 	// 使用根证书进行签名
 	cert, err := x509.CreateCertificate(rand, &template, rootCA, privateKey.Public(), rootCAKey)
 	if err != nil {
@@ -67,7 +74,7 @@ func main() {
 	}
 
 	// 将证书和私钥写入文件
-	certOut, err := os.Create("node.crt")
+	certOut, err := os.Create(fmt.Sprintf("node_%d.crt", nextNodeNum-1))
 	if err != nil {
 		log.Fatalf("Failed to open node.crt for writing: %v", err)
 	}
@@ -75,7 +82,7 @@ func main() {
 
 	pem.Encode(certOut, &pem.Block{Type: "CERTIFICATE", Bytes: cert})
 
-	keyOut, err := os.OpenFile("node.key", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+	keyOut, err := os.OpenFile(fmt.Sprintf("node_%d.key", nextNodeNum-1), os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
 	if err != nil {
 		log.Fatalf("Failed to open node.key for writing: %v", err)
 	}
